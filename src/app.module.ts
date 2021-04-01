d8d846a1dc1febb17bf0f855828bf3d0aaea4809
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-04-01 13:57:40 +0700

    [INIT] user and auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-	imports: [UserModule],
+	imports: [UserModule, UsersModule, AuthModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
