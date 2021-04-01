cd1726fa669f3cfe0c219c03070db23da7c5af5f
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-04-01 22:31:38 +0700

    ADD CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
 	const port = process.env.PORT || 3000
 	const app = await NestFactory.create(AppModule)
+	app.enableCors()
 	await app.listen(port)
 }
 bootstrap()
