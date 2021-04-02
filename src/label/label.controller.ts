64622dd7435f7577bf84ebb2e1fdddf891142f9d
GGolfz <wisarutgolf11@gmail.com>
2021-04-03 00:18:30 +0700

    update: label controller :sparkles:

--- src/label/label.controller.ts
+++ src/label/label.controller.ts
@@ -1,5 +1,16 @@
 import { Label, LabelType } from '.prisma/client'
-import { Body, Controller, Get, Param, Patch, Post, Request, UseGuards } from '@nestjs/common'
+import {
+	Body,
+	Controller,
+	Delete,
+	ForbiddenException,
+	Get,
+	Param,
+	Patch,
+	Post,
+	Request,
+	UseGuards,
+} from '@nestjs/common'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { LabelDto } from './dto/label.dto'
 import { LabelService } from './label.service'
@@ -29,8 +40,21 @@ export class LabelController {
 	async editLabel(@Param() params, @Body() { name, type }: LabelDto, @Request() req): Promise<Label[]> {
 		const userId = req.user.userId
 		const labelId = params.id
+		const isOwnLabel = await this.labelService.checkLabelOwnership(userId, labelId)
+		if (!isOwnLabel) throw new ForbiddenException()
 		const labelType: LabelType = await this.labelService.getLabelType(type)
 		await this.labelService.editLabel(labelId, name, labelType)
+		return this.labelService.getLabels(userId)
+	}
+
+	@Delete('label/:id')
+	@UseGuards(JwtAuthGuard)
+	async deleteLabel(@Param() params, @Request() req): Promise<Label[]> {
+		const userId = req.user.userId
+		const labelId = params.id
+		const isOwnLabel = await this.labelService.checkLabelOwnership(userId, labelId)
+		if (!isOwnLabel) throw new ForbiddenException()
+		await this.labelService.deleteLabel(labelId)
 		return this.labelService.getLabels(userId)
 	}
 }
