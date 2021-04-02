86de3dfdbcb255124e037fba6eb49816667addcb
GGolfz <wisarutgolf11@gmail.com>
2021-04-02 23:48:09 +0700

    add: label service :sparkles:

---
+++ src/label/label.service.ts
@@ -0,0 +1,45 @@
+import { Label, LabelType } from '.prisma/client'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+
+@Injectable()
+export class LabelService {
+	constructor(private prisma: PrismaService) {}
+	async createLabel(label: string, type: LabelType, ownerId: number): Promise<Label> {
+		return this.prisma.label.create({
+			data: {
+				label,
+				type,
+				owner_id: ownerId,
+			},
+		})
+	}
+
+	async getLabels(ownerId: number): Promise<Label[]> {
+		return this.prisma.label.findMany({
+			where: {
+				owner_id: ownerId,
+			},
+		})
+	}
+
+	async editLabel(labelId: number, label: string, type: LabelType): Promise<Label> {
+		return this.prisma.label.update({
+			data: {
+				label,
+				type,
+			},
+			where: {
+				id: labelId,
+			},
+		})
+	}
+	async getLabelType(type: string): Promise<LabelType> {
+		switch (type) {
+			case 'Income':
+				return LabelType.INCOME
+			case 'Expense':
+				return LabelType.EXPENSE
+		}
+	}
+}
