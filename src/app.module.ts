96472fce9cc371951387654eb7fa4b82fca21bb0
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-04-02 15:02:26 +0700

    Init wallet

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ConfigModule } from '@nestjs/config'
 import { TransactionService } from './transaction/transaction.service'
 import { TransactionModule } from './transaction/transaction.module'
 import { PrismaService } from './prisma.service'
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
 	imports: [
@@ -16,6 +17,7 @@ import { PrismaService } from './prisma.service'
 		UsersModule,
 		AuthModule,
 		TransactionModule,
+		WalletModule,
 	],
 	controllers: [AppController],
 	providers: [AppService, TransactionService, PrismaService],
