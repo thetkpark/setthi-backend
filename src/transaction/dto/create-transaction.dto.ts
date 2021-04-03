244e70318d9508fea28fb9522ce7c8ab159cc847
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-04-03 17:26:21 +0700

    [ADD] CreateTransactionDTO

---
+++ src/transaction/dto/create-transaction.dto.ts
@@ -0,0 +1,32 @@
+import { TransactionType } from '.prisma/client'
+import { IsDateString, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator'
+
+export class CreateTransactionDto {
+	@IsNotEmpty()
+	@IsString()
+	title: string
+
+	@IsNotEmpty()
+	@IsNumber()
+	amount: number
+
+	@IsNotEmpty()
+	@IsDateString()
+	date: Date
+
+	@IsNotEmpty()
+	@IsEnum(TransactionType)
+	transaction_type: TransactionType
+
+	@IsNotEmpty()
+	@IsNumber()
+	category_id: number
+
+	@IsNotEmpty()
+	@IsNumber()
+	wallet_id: number
+
+	@IsOptional()
+	@IsNumber()
+	saving_id: number
+}
