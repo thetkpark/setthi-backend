9e3c1c3f05aa8af1d03865cde9ad7fb29a760680
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-04-06 16:56:34 +0700

    [ADD] Wallet validation

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -17,6 +17,7 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { CreateWalletDto } from './dto/create-wallet.dto'
 import { EditWalletDto } from './dto/edit-wallet.dto'
 import { WalletService } from './wallet.service'
+import { WalletValidationPipe } from './wallet-validation.pipe'
 
 @Controller('api')
 export class WalletController {
@@ -41,22 +42,22 @@ export class WalletController {
 	@Patch('wallet/:id')
 	@UseGuards(JwtAuthGuard)
 	async editWallet(
-		@Param('id', ParseIntPipe) walletId: number,
+		@Param('id', WalletValidationPipe) wallet: Wallet,
 		@Body() { name }: EditWalletDto,
 		@Request() req
 	): Promise<Wallet[]> {
-		const isOwnWallet = await this.walletService.checkWalletOwnership(req.user.userId, walletId)
+		const isOwnWallet = await this.walletService.checkWalletOwnership(req.user.userId, wallet.id)
 		if (!isOwnWallet) throw new ForbiddenException()
-		await this.walletService.editWallet(walletId, name)
+		await this.walletService.editWallet(wallet.id, name)
 		return this.walletService.getWallets(req.user.userId)
 	}
 
 	@Delete('wallet/:id')
 	@UseGuards(JwtAuthGuard)
-	async deleteWallet(@Param('id', ParseIntPipe) walletId: number, @Request() req): Promise<Wallet[]> {
-		const isOwnWallet = await this.walletService.checkWalletOwnership(req.user.userId, walletId)
+	async deleteWallet(@Param('id', WalletValidationPipe) wallet: Wallet, @Request() req): Promise<Wallet[]> {
+		const isOwnWallet = await this.walletService.checkWalletOwnership(req.user.userId, wallet.id)
 		if (!isOwnWallet) throw new ForbiddenException()
-		await this.walletService.deleteWallet(walletId)
+		await this.walletService.deleteWallet(wallet.id)
 		return this.walletService.getWallets(req.user.userId)
 	}
 }
