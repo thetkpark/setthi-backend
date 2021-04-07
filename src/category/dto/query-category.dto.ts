7879cbd080d71eb8af8cac748226de710001cc9a
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-04-07 14:42:34 +0700

    Remove unused import

--- src/category/dto/query-category.dto.ts
+++ src/category/dto/query-category.dto.ts
@@ -1,5 +1,5 @@
 import { CategoryType } from '.prisma/client'
-import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator'
+import { IsEnum, IsOptional } from 'class-validator'
 
 export class QueryCategoryDto {
 	@IsOptional()
