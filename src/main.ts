70b6556665df8c70ea559fd7c9be4940d1031861
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-04-09 09:05:46 +0700

    INIT swagger OpenAPI

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
@@ -7,6 +8,14 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.enableCors()
 	app.useGlobalPipes(new ValidationPipe({ transform: true }))
+
+	const config = new DocumentBuilder()
+		.setTitle('Setthi Backend API')
+		.setDescription('An Swagger API Documentation for Setthi Backend API')
+		.build()
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('api', app, document)
+
 	await app.listen(port)
 }
 bootstrap()
